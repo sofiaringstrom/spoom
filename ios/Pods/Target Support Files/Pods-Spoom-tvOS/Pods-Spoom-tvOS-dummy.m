b52c57c177ec0eb01cbfaf5c019e0126ca03eabd
Sofia Ringström <sofia.ringstrom@live.se>
2021-02-01 11:43:39 +0100

    Added pods

---
+++ ios/Pods/Target Support Files/Pods-Spoom-tvOS/Pods-Spoom-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Spoom_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_Spoom_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-Spoom-tvOSTests/Pods-Spoom-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Spoom_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_Spoom_tvOSTests
+@end
